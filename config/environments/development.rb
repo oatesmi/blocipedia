3a2d4ac6c9ab6f1863e70679bada9913b75cb7dc
cddlslt <oatesmi.74@gmail.com>
2018-03-13 20:39:54 -0500

    don't use this commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: "blocipedia@gmail.com",
+    password: "Cddlslt2@",
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   get 'welcome/index'
 
   get 'welcome/about'
 
   root 'welcome#index'
 
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
