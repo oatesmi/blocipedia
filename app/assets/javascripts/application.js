2df5bedd02f187542399062af24c58c3d387eaf4
cddlslt <oatesmi.74@gmail.com>
2018-05-14 22:56:46 -0500

    added js to track in blocmetrics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,12 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+var blocmetrics = {};
+  blocmetrics.report = function(eventName){
+    var event = {event: { name: eventName }};
+    var request = new XMLHttpReqest();
+    request.open("POST", "http://localhost:3000/api/events", true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify(event));
+  };
